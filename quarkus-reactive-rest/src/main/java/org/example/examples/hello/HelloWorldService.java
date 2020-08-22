ca857b67517a99916d6b7a7fb2b0981c84334729
Thomas Andre <thomasandre84@gmail.com>
2020-08-22 18:47:37 +0200

    renice pom.xml files

--- quarkus-reactive-rest/src/main/java/org/example/examples/hello/HelloWorldService.java
+++ quarkus-reactive-rest/src/main/java/org/example/examples/hello/HelloWorldService.java
@@ -1,23 +1,13 @@
 package org.example.examples.hello;
 
-import examples.HelloReply;
-import examples.HelloRequest;
-import examples.MutinyGreeterGrpc;
 import io.smallrye.mutiny.Uni;
 
 import javax.inject.Singleton;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Singleton
-public class HelloWorldService extends MutinyGreeterGrpc.GreeterImplBase {
+public class HelloWorldService {
 
     AtomicInteger counter = new AtomicInteger();
 
-    @Override
-    public Uni<HelloReply> sayHello(HelloRequest request) {
-        int count = counter.incrementAndGet();
-        String name = request.getName();
-        return Uni.createFrom().item("Hello " + name)
-                .map(res -> HelloReply.newBuilder().setMessage(res).setCount(count).build());
-    }
 }
