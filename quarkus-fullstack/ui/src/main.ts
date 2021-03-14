20052c4cbaf65dc33832079308d494bbec62f642
Thomas Andre <thomasandre84@gmail.com>
2021-03-14 17:28:29 +0100

    add data for vuex

--- quarkus-fullstack/ui/src/main.ts
+++ quarkus-fullstack/ui/src/main.ts
@@ -2,12 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
-import store from './store'
+//import store from './store'
+import { store, key } from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 createApp(App)
-    .use(store)
+    .use(store, key)
     .use(router)
     .use(VueAxios, axios)
     .mount('#app')
