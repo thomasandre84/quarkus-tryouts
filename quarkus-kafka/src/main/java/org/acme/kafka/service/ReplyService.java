0997d18b029364a2dcc8a46b0344068fb9aa4786
Thomas Andre <thomasandre84@gmail.com>
2021-05-04 17:13:04 +0200

    getting timeout exception, which is thrown to dead-letter-queue

--- quarkus-kafka/src/main/java/org/acme/kafka/service/ReplyService.java
+++ quarkus-kafka/src/main/java/org/acme/kafka/service/ReplyService.java
@@ -1,10 +1,12 @@
 package org.acme.kafka.service;
 
+import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.kafka.IncomingKafkaRecordMetadata;
 import io.smallrye.reactive.messaging.kafka.OutgoingKafkaRecordMetadata;
 import lombok.extern.slf4j.Slf4j;
 import org.acme.kafka.interceptor.ConsumerTraceBinding;
 import org.acme.kafka.util.KafkaHeaderUtil;
+import org.eclipse.microprofile.context.ManagedExecutor;
 import org.eclipse.microprofile.opentracing.Traced;
 import org.eclipse.microprofile.reactive.messaging.*;
 
@@ -12,8 +14,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.time.LocalDateTime;
 import java.util.Optional;
-import java.util.Random;
-import java.util.concurrent.CompletionStage;
+import java.util.concurrent.*;
 
 @Slf4j
 @ApplicationScoped
@@ -22,22 +23,35 @@ public class ReplyService {
     @Channel("response")
     Emitter<String> responseEmitter;
 
+    @Inject
+    ManagedExecutor managedExecutor;
+
     @Incoming("request-in")
     @Traced
     @ConsumerTraceBinding
     public CompletionStage<Void> getRequest(Message<String> message) {
         String out = message.getPayload() + " " + LocalDateTime.now(); // The operation
-        if (new Random().nextBoolean()) {
-            sendOptionalResponse(out, message);
-            return message.ack();
+
+        Uni<Void> uni = null;
+        CompletableFuture<Void> ret = managedExecutor.runAsync(() -> {
+            try {
+                sendOptionalResponse(out, message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        try {
+            ret.get(2000l, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+            message.nack(e);
         }
-        else {
-            return message.nack(new Throwable("Test"));
-        }
+        return message.ack();
     }
 
-    private void sendOptionalResponse(String out, Message<String> message){
+    private void sendOptionalResponse(String out, Message<String> message) throws InterruptedException {
         Optional<IncomingKafkaRecordMetadata> metadata = message.getMetadata(IncomingKafkaRecordMetadata.class);
+        Thread.sleep(20000L);
         if (metadata.isPresent()) {
             Optional<Integer> targetPartition = KafkaHeaderUtil.getTargetPartition(metadata.get());
             Optional<String> id = KafkaHeaderUtil.getHeaderId(metadata.get());
